5ffc559873bff12062cebb0b15600b5e1845dd80
SergioEvando23 <sergioevandocosta@gmail.com>
2020-12-07 13:17:32 -0300

    header started, home button added, and background color changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import ListScreen from './pages/listScreen/ListScreen';
-import DetailScreen from './pages/detailScreen/DatailScreen'
+import DetailScreen from './pages/detailScreen/DatailScreen';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="app">
-      <div className="app__header"> 
-        app__reader
+      <div className="app__app-header"> 
+        <Button variant="contained" style={{padding:"0.2em"}}>Home</Button>
       </div>
       <div  className="app__body" >
         <ListScreen />
